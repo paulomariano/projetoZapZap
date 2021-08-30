5d4f642c50792e7633495a76c3db7fed75111648
Paulo Mariano <paulo_fmariano@hotmail.com>
2021-08-30 20:50:26 +0000

    versão telefone

--- script.js
+++ script.js
@@ -9,4 +9,12 @@ $(document).ready(function(){
       window.location.href = `https://api.whatsapp.com/send?phone=+55${numero}`
     } 
   })
+  $('.form-check-input').click(function(){
+    if($('.form-check-input:checked').val() == "telefone"){
+      $('#phone').attr("pattern","\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4}")
+    } else {
+      $('#phone').attr("pattern","\([0-9]{2}\)[\s][0-9]{5}-[0-9]{4}")
+    }
+  })
+  
 })
