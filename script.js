34ae0fb6bcfc348af962b25358f666d46f37b33e
Paulo Mariano <paulo_fmariano@hotmail.com>
2021-09-01 00:21:36 +0000

    ajustes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Desenvolvido por Paulo Mariano
-// dentro de string \ significa entrada de unicode, para utilizar o simbolo
-// \ unicode equivalente \u005c
+// dentro de string \ significa entrada de unicode, para utilizar 
+// o simbolo \ unicode equivalente \u005c
 $(document).ready(function(){
   $('#phone').mask('(00) 00000-0000')
 
   $('#form-zapear').submit(function(event){
     event.preventDefault()
-    var numero = $('#phone').val()
+    const numero = $('#phone').val()
     if(numero != "" || numero != null){
       window.location.href = `https://api.whatsapp.com/send?phone=+55${numero}`
     } 
   })
+
   $('.form-check-input').click(function(){
     if($('.form-check-input:checked').val() == "telefone"){
       $('#phone').attr("pattern","\u005c([0-9]{2}\u005c)[\u005cs][0-9]{4}-[0-9]{4}")
@@ -22,5 +23,5 @@ $(document).ready(function(){
       $('#phone').val('')
     }
   })
-  
+
 })
